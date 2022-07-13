f418a6e5cfec90033a8a51f1ce35e4e317720ccd
xiguang <1046532637@qq.com>
2022-07-13 10:16:51 +0800

    Update camera matrix

--- modules/predictor-armor/spin_detector.cpp
+++ modules/predictor-armor/spin_detector.cpp
@@ -22,20 +22,20 @@ bool SpinDetector::Update(const Armor &armor, const uint64_t current_time) {
         is_quick_spin_ = false;
     }
 
-    double current_yaw_x, yaw_delta;
+    double current_yaw_x, yaw_x_delta;
     if(mode_ == Mode::kSpherical){
         current_yaw_x = std::atan2(armor.TranslationVectorWorld()(0, 0),
                                    armor.TranslationVectorWorld()(2,0));
-        yaw_delta = algorithm::shortest_angular_distance(last_yaw_x_, current_yaw_x);
+        yaw_x_delta = algorithm::shortest_angular_distance(last_yaw_x_, current_yaw_x);
     }else{
         current_yaw_x = armor.Center().x;
-        yaw_delta = current_yaw_x - last_yaw_x_;
+        yaw_x_delta = current_yaw_x - last_yaw_x_;
     }
 
-    if(abs(yaw_delta) > min_jump_yaw_x_){
+    if(abs(yaw_x_delta) > min_jump_yaw_x_){
         reverse_buffer = 0;
         ++jump_count_;
-        if(jump_count_ > 1 && std::signbit(last_yaw_x_delta_) == std::signbit(yaw_delta)){
+        if(jump_count_ > 1 && std::signbit(last_yaw_x_delta_) == std::signbit(yaw_x_delta)){
             if(time_after_last_jump > quick_jump_period_max_ && time_after_last_jump < slow_jump_period_max_) {
                 is_slow_spin_ = true;
                 is_quick_spin_ = false;
@@ -47,16 +47,18 @@ bool SpinDetector::Update(const Armor &armor, const uint64_t current_time) {
             }
             // if time_after_last_jump < quick_jump_period_min_, consider it that flicker.
         }
-        if(std::signbit(yaw_delta)) // if yaw_delta is negative.
+        if(std::signbit(yaw_x_delta)) // if yaw_delta is negative.
             clockwise_ = 0;
         else
             clockwise_ = 1;
         last_jump_time_ = current_time;
-        last_yaw_x_delta_ = yaw_delta;
+        last_yaw_x_delta_ = yaw_x_delta;
         last_jump_position_ = armor.TranslationVectorWorld();
-    }else if(std::signbit(yaw_delta) == std::signbit(last_yaw_x_delta_)){   // not jump but yaw delta has the same direction.
+    }else if(std::signbit(yaw_x_delta) == std::signbit(last_yaw_x_delta_)){   // not jump but yaw delta has the same direction.
         ++reverse_buffer;
         if(reverse_buffer > kMaxReverseSpinBuffer){
+            static int time = 0;
+            DLOG(INFO) << "reverse_buffer: " << ++time;
             reverse_buffer = 0;
             jump_count_ = 0;
             is_quick_spin_ = is_slow_spin_ = false;
