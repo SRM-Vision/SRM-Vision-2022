f3ad279e10949d81e8638628829bb7899b971219
screw <xinyu-44@outlook.com>
2022-07-15 09:31:13 +0800

    add documentation in controller-deb , also rename some of the funtions.

--- modules/debug-tools/controller-debug.h
+++ modules/debug-tools/controller-debug.h
@@ -9,9 +9,18 @@
 #include "parameter-maintain/parameter-maintain.h"
 #include "predictor-armor/predictor_armor.h"
 
+/**
+ * \brief This Class is used as the tool class for individual controllerDebug class.
+ * \attention You may add new functions for general debug, but functions needs to be inline void.
+ */
 class ControllerDebug
 {
 public:
+    /**
+     * \brief For manually initialize the Debug tools.
+     * \param use_painter
+     * \note param:debug is replaced by #if !NDEBUG
+     */
     void Initialize(const bool use_painter)
     {
         if (use_painter)
@@ -25,27 +34,42 @@ public:
     }
 
 public:
+    /// Painter for drawing
     debug::IPainter* painter_;
 
 public:
-
-    inline void UpdateImage(const cv::Mat& image)
+    /**
+     * \brief Update the image for showing, Remember to update every time you want to draw sth..
+     * \param image
+     */
+    inline void UpdateImage(const cv::Mat& image) const
     {
         painter_->UpdateImage(image);
     }
 
 
-    inline void drawROI(const cv::Rect_<int>& ROI)
+    /**
+     * \brief Draw roi in the frame
+     * \param roi
+     */
+    inline void DrawRoi(const cv::Rect_<int>& roi) const
     {
-        painter_->DrawBoundingBox(ROI, cv::Scalar(0, 0, 255), 2);
+        painter_->DrawBoundingBox(roi, cv::Scalar(0, 0, 255), 2);
     }
 
-    inline void drawArmors(const std::vector<bbox_t> &bboxes,
+    /**
+     * \brief Draw detected armors results from the armor_detector.
+     * \param boxes
+     * \param armor_predictor
+     * \param intrinsic_matrix
+     * \param image_size
+     */
+    inline void DrawArmors(const std::vector<bbox_t> &boxes,
                            ArmorPredictor* armor_predictor,
                            const cv::Mat &intrinsic_matrix,
-                           const cv::MatSize &image_size)
+                           const cv::MatSize &image_size) const
     {
-        for (const auto &box: bboxes) {
+        for (const auto &box: boxes) {
             painter_->DrawRotatedRectangle(box.points[0],
                                            box.points[1],
                                            box.points[2],
@@ -58,16 +82,29 @@ public:
         painter_->DrawPoint(armor_predictor->TargetCenter(), cv::Scalar(100, 255, 100), 2, 2);
     }
 
+    /**
+     * \brief Draw text in the frame
+     * \param text
+     */
     inline void ShowText(const std::string &text) const
     {
         painter_->DrawText(text,{200,200},{0,0,255},2);
     }
 
+    /**
+     * \brief Draw point.
+     * \param point
+     */
     inline void ShowPoint(const cv::Point2f& point) const
     {
         painter_->DrawPoint(point,{0,255,0},5,2);
     }
 
+    /**
+     * \brief Show drawn image, Remember to do this as the last step for drawing.
+     * \param window_names
+     * \param wait_time
+     */
     inline void ShowImage(const std::string &window_names, const int &wait_time) const
     {
         painter_->ShowImage(window_names, wait_time);
