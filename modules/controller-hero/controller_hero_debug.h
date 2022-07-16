12319197487f875e9ec1ce61d0124bb2b918a440
lzy20020320 <1016984801@qq.com>
2022-07-16 22:01:31 +0800

    Outpost的newrun修改bug，hero_debug中outpost显示修改

--- modules/controller-hero/controller_hero_debug.h
+++ modules/controller-hero/controller_hero_debug.h
@@ -7,10 +7,9 @@
 #include "debug-tools/controller-debug.h"
 #include "controller_hero_debug.h"
 
-class ControllerHeroDebug{
+class ControllerHeroDebug {
 public:
-    void Initialize(const bool use_painter)
-    {
+    void Initialize(const bool use_painter) {
         controller_debug_.Initialize(use_painter);
     }
 
@@ -18,43 +17,48 @@ private:
     ControllerDebug controller_debug_;
 
 public:
-    inline void DrawArmorDetection(const cv::Mat& image,
-                                   const cv::Rect_<int>& ROI,
+    inline void DrawArmorDetection(const cv::Mat &image,
+                                   const cv::Rect_<int> &ROI,
                                    const std::vector<bbox_t> &bboxes,
-                                   ArmorPredictor* armor_predictor,
+                                   ArmorPredictor *armor_predictor,
                                    const cv::Mat &intrinsic_matrix,
                                    const cv::MatSize &image_size,
                                    const std::string &window_names,
-                                   const int &wait_time)
-    {
+                                   const int &wait_time) {
         controller_debug_.UpdateImage(image);
         controller_debug_.DrawRoi(ROI);
         controller_debug_.DrawArmors(bboxes, armor_predictor, intrinsic_matrix, image_size);
         controller_debug_.ShowImage(window_names, wait_time);
+
     }
 
-    inline void DrawOutpostData(const cv::Mat& image,
-                                   const cv::Rect_<int>& ROI,
-                                   OutpostPredictor* outpost_predictor,
-                                   const cv::Mat &intrinsic_matrix,
-                                   const cv::MatSize &image_size,
-                                   const std::string &window_names,
-                                   const int &wait_time)
-    {
+
+    inline void DrawOutpostData(const cv::Mat &image,
+                                const cv::Rect_<int> &ROI,
+                                OutpostPredictor *outpost_predictor,
+                                const cv::Mat &intrinsic_matrix,
+                                const cv::MatSize &image_size,
+                                const std::string &window_names,
+                                const int &wait_time) {
         controller_debug_.UpdateImage(image);
         controller_debug_.DrawRoi(ROI);
         controller_debug_.ShowPoint(outpost_predictor->OutpostCenter());
-        if(outpost_predictor->Fire())
+        if (outpost_predictor->Fire())
             controller_debug_.ShowText("Fire");
+
+        controller_debug_.painter_->DrawLine({float(image_size().width *0.5 ),50},
+                                             {float(image_size().width *0.5), float(image.size().height - 50)},
+                                             cv::Scalar(255,0, 255),
+                                             3);
         controller_debug_.ShowImage(window_names, wait_time);
+
+
     }
 
-    inline char GetKey()
-    {
+    inline char GetKey() {
 #if !NDEBUG
-        char key = cv::waitKey(1) & 0xff;
-        if (key == 's')
-        {
+        char key = cv::waitKey(6) & 0xff;
+        if (key == 's') {
             ArmorPredictorDebug::Instance().Save();
             OutpostPredictorDebug::Instance().Save();
 
@@ -67,7 +71,6 @@ public:
         return 'h';
     }
 };
-
 
 
 #endif //CONTROLLER_INFANTRY_DEBUG_H_
