085c38f3e0621910bf174067e39c7e369fdc590f
screw <xinyu-44@outlook.com>
2022-07-24 10:32:37 +0800

    predictor rune kalman first push, only tested in debug / video

--- modules/predictor-rune-kalman/predictor_rune_kalman.h
+++ modules/predictor-rune-kalman/predictor_rune_kalman.h
@@ -8,6 +8,7 @@
 #include "lang-feature-extension/disable-constructors.h"
 #include "digital-twin/facilities/power_rune.h"
 #include "data-structure/communication.h"
+#include "math-tools/ekf.h"
 
 class RunePredictorKalman : NO_COPY, NO_MOVE {
 public:
@@ -16,13 +17,18 @@ public:
 
     ~RunePredictorKalman() = default;
 
-    bool Initialize(const std::string &config_path);
+    bool Initialize();
 
     [[nodiscard]]SendPacket Run(const PowerRune &power_rune, AimModes aim_modes, float bullet_speed);
 
 private:
+    ExtendedKalmanFilter<5,1> ekf_;
 
+    double last_rad;
 
+    double rotate_speed = -1141514.65742;
+
+    void InitializeEKF(double rad);
 };
 
 #endif //PREDICTOR_RUNE_KALMAN_H_
