49252db66d8444d5fb9d077ee283f474fd7fd7dd
wangyw15 <wangyanwei2012@sina.com>
2023-03-29 12:56:16 +0800

    Yolo rune detector

--- modules/detector-rune-yolo/detector_rune_yolo.cpp
+++ modules/detector-rune-yolo/detector_rune_yolo.cpp
@@ -1,33 +1,148 @@
-#include "detector_rune_yolo.h"
+/*
+    Create by wangyw15 on 29.03.2023
+    Detect rune through yolo network
+    Uses yolo-network moudle
+*/
 
-#include <opencv2/opencv.hpp>
-#include <vector>
-#include <algorithm>
+#include "detector_rune_network.h"
+#include <filesystem>
+#include <fstream>
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+#include <NvOnnxParser.h>
+#include <logger.h>
+#include <glog/logging.h>
+#include <Eigen/Eigen>
 
-RuneDetectorYolo::RuneDetectorYolo()
+#define TRT_ASSERT(expr)                                              \
+    if(!(expr)) {                                                     \
+        LOG(ERROR) << "TensorRT assertion failed: " << #expr << ".";  \
+        exit(-1);                                                     \
+    }
+
+static inline int argmax(const float *ptr, int len)
 {
-    _Network = YoloNetwork("yolo-rune.onnx", 2, 5);
+    int max_arg = 0;
+    for (int i = 1; i < len; i++) {
+        if (ptr[i] > ptr[max_arg]) max_arg = i;
+    }
+    return max_arg;
+}
+
+static inline size_t get_dims_size(const nvinfer1::Dims &dims) {
+    size_t sz = 1;
+    for (int i = 0; i < dims.nbDims; ++i) sz *= dims.d[i];
+    return sz;
+}
+
+
+bool RuneDetectorYolo::Initialize(const std::string &onnx_file) {
+    yolo_network_ = YoloNetwork(onnx_file, 2, 5);
+    
+    return true;
+}
+
+RuneDetectorYolo::~RuneDetectorYolo() {
     
 }
 
-RuneDetectorYolo::~RuneDetectorYolo()
-{
-    
+PowerRune RuneDetectorYolo::Run(Entity::Colors color, Frame &frame) {
+    // calculate run time
+    auto current_time_chrono = std::chrono::high_resolution_clock::now();
+    current_time_ = double(std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now().time_since_epoch()).count());
+    time_gap_ = (static_cast<std::chrono::duration<double, std::milli>>(
+            current_time_chrono - last_time_)).count();
+    last_time_ = current_time_chrono;
+    current_time_ /= 1000;
+
+    // inference
+    auto result = yolo_network_.Inference(frame.image);
+
+    // empty result
+    if (result.empty())
+    {
+        return PowerRune();
+    }
+
+    // mean filter to get stable center R
+    if(abs(result[0].pts[3].x - energy_center_r_.x) < 100)
+        energy_center_r_ = result[0].pts[3] / 2 + energy_center_r_ / 2;
+    else
+        energy_center_r_ = result[0].pts[3];
+
+    // calculate the center of armor
+    armor_center_p_ = cv::Point2f(0, 0);
+    for (int i = 0; i < 5; i++)
+    {
+        if (i == 3)
+            continue;
+        armor_center_p_ += result[0].pts[i];
+    }
+    armor_center_p_ /= 4;
+    rtp_vec_ = armor_center_p_ - energy_center_r_;
+
+    if (!clockwise_)
+        FindRotateDirection();
+
+//    if (armor_center_p_ != cv::Point2f(0, 0))
+//    {
+//        time_gap_ = static_cast<double>(frame.time_stamp * 1e-6) - current_time_;
+//        current_time_ = static_cast<double>(frame.time_stamp * 1e-6);
+//        DLOG(INFO) << "current time" << current_time_;
+//    }
+
+    return {color,
+            clockwise_,
+            time_gap_,
+            current_time_,
+            rtp_vec_,
+            energy_center_r_,
+            armor_center_p_,
+            cv::Point2f(float(frame.image.cols >> 1), float(frame.image.rows >> 1))};
 }
 
-PowerRune RuneDetectorYolo::Inference(cv::Mat image)
-{
-    auto result = _Network.Inference()
-    PowerRune rune;
-    rune.rx = result[0].pts[3].x;
-    rune.ry = result[0].pts[3].y;
-    // calculate center point
-    rune.x = (result[0].pts[0].x + result[0].pts[1].x + result[0].pts[2].x + result[0].pts[4].x) / 4;
-    rune.y = (result[0].pts[0].y + result[0].pts[1].y + result[0].pts[2].y + result[0].pts[4].y) / 4;
-    // calculate width and height
-    rune.w = std::max({ result[0].pts[0].x, result[0].pts[1].x, result[0].pts[2].x, result[0].pts[4].x }) -
-        std::min({ result[0].pts[0].x, result[0].pts[1].x, result[0].pts[2].x, result[0].pts[4].x });
-    rune.h = std::max({ result[0].pts[0].y, result[0].pts[1].y, result[0].pts[2].y, result[0].pts[4].y }) -
-        std::min({ result[0].pts[0].y, result[0].pts[1].y, result[0].pts[2].y, result[0].pts[4].y });
-    return rune;
+void RuneDetectorYolo::FindRotateDirection() {
+    static std::vector<cv::Point2f> r_to_p_vec;  ///< Vector Used for deciding rotation direction.
+    const float kMaxRatio = 0.1;
+    static float rune_radius_ = 120;
+
+    // detection is not found.
+    if (rtp_vec_ == cv::Point2f(0, 0))
+        return;
+
+    if (clockwise_ == 0) {
+        r_to_p_vec.emplace_back(rtp_vec_);
+    }
+    // note:frame lost is deleted
+
+    if (clockwise_ == 0 && static_cast<int>(r_to_p_vec.size()) > 20)
+    {
+        cv::Point2f first_rotation = r_to_p_vec[5];  // The fist five frames may be invalid.
+        float radius, final_radius = 0;
+        for (auto current_rotation = r_to_p_vec.begin() + 6; current_rotation != r_to_p_vec.end(); ++current_rotation) {
+            double cross = first_rotation.cross(cv::Point2f(current_rotation->x, current_rotation->y));
+            radius = algorithm::SqrtFloat(
+                    current_rotation->x * current_rotation->x + current_rotation->y * current_rotation->y);
+            final_radius +=
+                    std::min(rune_radius_ * (1 + kMaxRatio), std::max(rune_radius_ * (1 - kMaxRatio), radius)) / 15;
+
+            if (cross > 0.0)
+                ++clockwise_;
+            else if (cross < 0.0)
+                --clockwise_;
+        }
+        if (clockwise_ > 8) {
+            DLOG(INFO) << "Power rune's direction is clockwise." << " \tRadius: " << rune_radius_;
+            clockwise_ = 1;
+        } else if (clockwise_ < -8) {
+            DLOG(INFO) << "Power rune's direction is anti-clockwise." << " \tRadius: " << rune_radius_;
+            clockwise_ = -1;
+        } else {
+            clockwise_ = 0;
+            DLOG(WARNING) << "Rotating direction is not decided!" << " \tRadius: " << rune_radius_;
+            r_to_p_vec.clear();
+        }
+        rune_radius_ = final_radius;
+    }
 }
--- modules/detector-rune-yolo/detector_rune_yolo.h
+++ modules/detector-rune-yolo/detector_rune_yolo.h
@@ -1,34 +1,87 @@
 /*
-    Create by wangyw15 on 25.03.2023
+    Create by wangyw15 on 29.03.2023
     Detect rune through yolo network
     Uses yolo-network moudle
 */
 #ifndef DETECTOR_RUNE_YOLO_H_
 #define DETECTOR_RUNE_YOLO_H_
 
+#include <NvInfer.h>
+#include <lang-feature-extension/disable-constructors.h>
+#include "digital-twin/facilities/power_rune.h"
+#include "data-structure/frame.h"
+#include <lang-feature-extension/attr-reader.h>
+#include <opencv2/opencv.hpp>
 #include "../yolo-network/yolo_network.h"
 
-#include <string>
-
-struct PowerRune
+struct BuffObject
 {
-    // The big R
-    float rx, ry;
-    // The center of the object
-    float x, y;
-    // The size of the object
-    float h, w;
+    cv::Point2f apex[5];
+    cv::Rect_<float> rect;
+    int cls;
+    int color;
+    float prob;
+    std::vector<cv::Point2f> pts;
 };
 
-class RuneDetectorYolo
-{
-protected:
-    YoloNetwork _Network;
+class RuneDetectorYolo : NO_COPY, NO_MOVE {
+    static constexpr int kTopkNum = 128;
+    static constexpr float kKeepThreshold = 0.1f;
+    static constexpr int INPUT_W = 416;    // Width of input
+    static constexpr int INPUT_H = 416;    // Height of input
+    static constexpr int NUM_CLASSES = 2;  // Number of classes
+    static constexpr int NUM_COLORS = 2;   // Number of color
+    static constexpr int TOPK = 32;       // TopK
+    static constexpr float NMS_THRESH  = 0.1;
+    static constexpr float BBOX_CONF_THRESH = 0.95;
+    static constexpr float MERGE_CONF_ERROR = 0.15;
+    static constexpr float MERGE_MIN_IOU = 0.2;
 
 public:
-    RuneDetectorYolo();
-    virtual ~RuneDetectorYolo();
-    PowerRune Inference();
+    RuneDetectorYolo() : engine_(),
+                            context_(),
+                            device_buffer_(),
+                            output_buffer_(),
+                            stream_(),
+                            input_index_(),
+                            output_index_(),
+                            input_size_(),
+                            output_size_() {}
+
+    ~RuneDetectorYolo();
+
+    /**
+     * \brief Load and initialize model.
+     * \param [in] onnx_file ONNX file path.
+     */
+    bool Initialize(const std::string &onnx_file);
+
+
+
+    /**
+     * \brief Run detector once.
+     * \param [in] frame Input original frame.
+     * \return Output power rune data.
+     */
+    [[nodiscard]] PowerRune Run(Entity::Colors color, Frame &frame);
+
+    // model part
+private:
+    YoloNetwork yolo_network_;
+
+    // traditional part
+private:
+    cv::Point2f energy_center_r_;
+    cv::Point2f armor_center_p_;
+    cv::Point2f rtp_vec_;
+    double time_gap_{};  ///< Time gap between two valid frames.
+    double current_time_{};
+    std::chrono::high_resolution_clock::time_point last_time_{};  ///< Last frame's time.
+
+    int clockwise_ = 0;
+
+    void FindRotateDirection();
 };
 
-#endif
+
+#endif // DETECTOR_RUNE_YOLO_H_
